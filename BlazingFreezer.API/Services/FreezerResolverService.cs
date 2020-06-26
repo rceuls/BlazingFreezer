d3fba9f9031e47d6a1938ed0cace4e50f2542ba9
Raf Ceuls <rceuls@gmail.com>
2020-06-27 00:28:47 +0200

    feat: item retrieval and debug hookup

--- BlazingFreezer.API/Services/FreezerResolverService.cs
+++ BlazingFreezer.API/Services/FreezerResolverService.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlazingFreezer.API.Models;
 using Grpc.Core;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace BlazingFreezer.API.Services
@@ -13,6 +15,35 @@ namespace BlazingFreezer.API.Services
         public FreezerResolverService(IMongoService mongoService)
         {
             _mongoService = mongoService;
+        }
+
+        private static IEnumerable<FreezerDetailsItem> ConvertToItemList(FreezerMongoModel freezerMongoModel)
+        {
+            return (from drawer in (freezerMongoModel.Drawers ?? new FreezerDrawerMongoModel[0])
+                from item in (drawer.Items ?? new FreezerItemMongoModel[0])
+                select new FreezerDetailsItem()
+                {
+                    Name = item.Name,
+                    Since = item.Since.ToShortDateString(),
+                    DrawerId = drawer.Id.ToString(),
+                    IsVacuum = item.IsVacuum,
+                    ItemId = item.Id.ToString()
+                }).ToList();
+        }
+
+
+        public override async Task<FreezerDetailsReply> GetFreezerDetails(FreezerDetailsRequest request, ServerCallContext context)
+        {
+            var projection = new FindExpressionProjectionDefinition<FreezerMongoModel, IEnumerable<FreezerDetailsItem>>(p => ConvertToItemList(p));
+            var items = await _mongoService
+                .GetDatabase()
+                .GetCollection<FreezerMongoModel>("freezers")
+                .Find(x => x.Id == ObjectId.Parse(request.FreezerId))
+                .Project(projection)
+                .ToListAsync();
+            var ret = new FreezerDetailsReply();
+            ret.Items.AddRange(items.SelectMany(x => x).ToList());
+            return ret;
         }
 
         public override async Task<FreezerOverviewReply> GetFreezerOverview(FreezerOverviewRequest request,
